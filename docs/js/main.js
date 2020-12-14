5b89fc24137ce03d9d4ea091e13730c7caeca933
Vladimir <wolf-ls@mail.ru>
2020-12-14 13:57:40 +0300

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        navSpeed:1000,
+        nav: true,
+        loop: true
+        //   navText: ['Назад', 'Вперед']
+    });
+  });
